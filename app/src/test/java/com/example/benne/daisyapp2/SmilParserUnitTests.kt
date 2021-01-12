f93f6c64fd5118aca795361bbfa79965d9beac52
Matthew <mldelaney94@gmail.com>
2021-01-12 23:12:35 +1100

    regressed junit from 5.7.0 to 4.12

--- app/src/test/java/com/example/benne/daisyapp2/SmilParserUnitTests.kt
+++ app/src/test/java/com/example/benne/daisyapp2/SmilParserUnitTests.kt
@@ -1,17 +1,20 @@
 package com.example.benne.daisyapp2
 
+import com.example.benne.daisyapp2.data.daisy202.SmilParElement
 import com.example.benne.daisyapp2.data.daisy202.SmilParser.Companion.parseSmil
 import org.junit.BeforeClass
 import java.io.File
+import java.io.FileNotFoundException
 
 class SmilParserUnitTests {
     companion object {
         private val path = File("").absolutePath + "\\src\\test\\java\\com\\example\\benne\\daisyapp2"
+        private var smilElements: List<SmilParElement>? = null
         @BeforeClass
         @JvmStatic fun smilTestSetup() {
             try {
                 val file = File(path + "\\smilParserTestFile.html")
-                book = parseSmil(file)
+                smilElements = parseSmil(file.toString())
             } catch (ffe: FileNotFoundException) {
                 println(ffe.message)
             }
